b9809a22634891d1d29f49d1a9b6a84fe375204c
pmiglio21 <92951689+pmiglio21@users.noreply.github.com>
2024-01-08 18:14:48 -0500

    Commented out overworld level script
    - Moved map over a bit

--- Levels/OverworldLevels/Utilities/PlayerInstancer.cs
+++ Levels/OverworldLevels/Utilities/PlayerInstancer.cs
@@ -7,6 +7,9 @@ namespace Levels.OverworldLevels.Utilities
 {
 	public partial class PlayerInstancer : Node
 	{
+		private Vector2 _tempSpawnPosition = new Vector2(303, 181);
+
+
 		public override void _Ready()
 		{
 			//Going to have to find a way to not dispose of the characters every time, keep them global somehow
@@ -15,22 +18,24 @@ namespace Levels.OverworldLevels.Utilities
 			{
 				GD.Print($"P: {character.PlayerNumber}, D: {character.DeviceIdentifier}, C: {character.CharacterClassName}");
 
-				if (character.PlayerNumber == 0)
-				{
-					character.GlobalPosition = new Vector2(-15, -15);
-				}
-				else if (character.PlayerNumber == 1)
-				{
-					character.GlobalPosition = new Vector2(15, -15);
-				}
-				else if (character.PlayerNumber == 2)
-				{
-					character.GlobalPosition = new Vector2(-15, 15);
-				}
-				else if (character.PlayerNumber == 3)
-				{
-					character.GlobalPosition = new Vector2(15, 15);
-				}
+				character.GlobalPosition = _tempSpawnPosition;
+
+				//if (character.PlayerNumber == 0)
+				//{
+				//	character.GlobalPosition = new Vector2(50, 50);
+				//}
+				//else if (character.PlayerNumber == 1)
+				//{
+				//	character.GlobalPosition = new Vector2(50, 50);
+				//}
+				//else if (character.PlayerNumber == 2)
+				//{
+				//	character.GlobalPosition = new Vector2(-15, 15);
+				//}
+				//else if (character.PlayerNumber == 3)
+				//{
+				//	character.GlobalPosition = new Vector2(15, 15);
+				//}
 
 				AddChild(character);
 			}
