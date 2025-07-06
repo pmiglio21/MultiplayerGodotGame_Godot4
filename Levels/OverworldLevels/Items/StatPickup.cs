fbc41bddfcab2b46d02e93a27aff845bbd198261
pmiglio21 <92951689+pmiglio21@users.noreply.github.com>
2025-07-06 12:16:29 -0400

    StatPickups gravitate towards player

--- Levels/OverworldLevels/Items/StatPickup.cs
+++ Levels/OverworldLevels/Items/StatPickup.cs
@@ -1,14 +1,23 @@
 using Enums;
 using Godot;
-
+using MobileEntities.PlayerCharacters;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class StatPickup : Node2D
 {
-	private AnimationPlayer _animationPlayer { get; set; }
+    #region Components 
 
+    private AnimationPlayer _animationPlayer { get; set; }
+    protected Area2D _attractionArea;
+    protected CollisionShape2D _collisionShape;
 
-	public StatType StatType 
+    #endregion
+
+    private List<BaseCharacter> _playersInArea = new List<BaseCharacter>();
+
+    public StatType StatType 
 	{ 
 		get { return _statType;  }
 
@@ -49,12 +58,93 @@ public partial class StatPickup : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-	   _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-	}
+	    _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        _attractionArea = GetNode<Area2D>("AttractionArea");
+        _collisionShape = _attractionArea.GetNode<CollisionShape2D>("CollisionShape2D");
+    }
+
+    private BaseCharacter _nearestCharacter = null;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+        if (_playersInArea.Count > 0)
+        {
+            float minDistance = float.MaxValue;
 
-	}
+            foreach (BaseCharacter character in _playersInArea)
+            {
+                float newDistance = character.GlobalPosition.DistanceTo(GlobalPosition);
+
+                if (newDistance < minDistance)
+                {
+                    _nearestCharacter = character;
+
+                    minDistance = newDistance;
+                }
+            }
+        }
+        else
+        {
+            _nearestCharacter = null;
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (_nearestCharacter != null)
+        {
+            Vector2 newDistance = -.5f * _nearestCharacter.GlobalPosition.DirectionTo(GlobalPosition);
+
+            Translate(newDistance);
+        }
+    }
+
+    private void OnAttractionAreaEntered(Area2D area)
+    {
+        if (area.IsInGroup("PlayerHurtBox"))
+        {
+            CollisionShape2D collisionShape = area.GetNode<CollisionShape2D>("CollisionShape");
+
+            if (!collisionShape.Disabled)
+            {
+                BaseCharacter character = area.GetParent() as BaseCharacter;
+
+                if (!_playersInArea.Any(x => x == character))
+                {
+                    _playersInArea.Add(character);
+
+                    GD.Print($"PlayerHurtBox Entered {area.GetParent().Name}");
+                }
+            }
+        }
+    }
+
+    private void OnAttractionAreaExited(Area2D area)
+    {
+        if (area.IsInGroup("PlayerHurtBox"))
+        {
+            CollisionShape2D collisionShape = area.GetNode<CollisionShape2D>("CollisionShape");
+
+            if (!collisionShape.Disabled)
+            {
+                BaseCharacter character = area.GetParent() as BaseCharacter;
+
+                if (_playersInArea.Any(x => x == character))
+                {
+                    _playersInArea.Remove(character);
+
+                    GD.Print($"PlayerHurtBox Exited {area.GetParent().Name}");
+                }
+            }
+        }
+    }
+
+    private void OnPickupAreaEntered(Area2D area)
+    {
+        if (area.IsInGroup("PlayerHurtBox"))
+        {
+            this.QueueFree();
+        }
+    }
 }
